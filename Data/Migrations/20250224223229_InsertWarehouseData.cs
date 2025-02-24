91efe16eda4a17b450d60cfc6f04d325dc4fad58
José Luis Terán Rocha <jose.luis.teran.rocha.2@gmail.com>
2025-02-24 18:51:01 -0400

    chore(migrations): add initial data for tables

---
+++ Data/Migrations/20250224223229_InsertWarehouseData.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InventoryService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InsertWarehouseData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sqlFilePath = Path.Combine("Persistence/Database/Scripts", "002_InsertTableData.sql");
+            var sql = File.ReadAllText(sqlFilePath);
+            migrationBuilder.Sql(sql);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
