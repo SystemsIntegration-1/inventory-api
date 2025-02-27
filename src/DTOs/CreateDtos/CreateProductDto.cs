0c8a8984964cfd8e2e648cd54ba53106ea17b35b
just-fabian <fabian130404@gmail.com>
2025-02-27 11:46:20 -0400

    chore: delete stock and dates from createproductdto

--- src/DTOs/CreateDtos/CreateProductDto.cs
+++ src/DTOs/CreateDtos/CreateProductDto.cs
@@ -5,8 +5,5 @@ public class CreateProductDto
     public required string Name { get; set; }
     public required string Description { get; set; }
     public required string Category { get; set; }
-    public int AvailableQuantity { get; set; }
     public required string WarehouseLocation { get; set; }
-    public long EntryDate { get; set; }
-    public long ExpirationDate { get; set; }
 }
